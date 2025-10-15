8c79791431cf6d2120804ab48df0f2fbb2841a78
Cruz000Testings <cruz000testing@outlook.com>
2025-10-14 21:12:03 -0600

    Changes

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Cruz000Testing/go-ws
 
 go 1.25.2
+
+require github.com/gorilla/websocket v1.5.3
--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 
 func main() {
 	http.HandleFunc("/ws", wsHandler)
@@ -13,5 +24,20 @@ func main() {
 }
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		fmt.Println("Error updating connection", err)
+		return
+	}
 
+	defer ws.Close()
+
+	for {
+		_, _, err := ws.ReadMessage()
+
+		if err != nil {
+			ws.Close()
+			break
+		}
+	}
 }
